7afc24414599619c8562eb6353154d300b90a540
Harry <harrychenhc@icloud.com>
2021-02-19 22:41:55 -0500

    add a DataManagerComponent

--- app/components/date-manager.js
+++ app/components/date-manager.js
@@ -1,4 +1,10 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class DateManagerComponent extends Component {
+  @tracked date = new Date();
+
+  get localTime() {
+    return this.date;
+  }
 }
